bf54d50806d6ca6b752570af3095a7d2b8f0aa6d
Amanda-Fajardo <amandafajardo.dev@gmail.com>
2026-06-04 17:12:32 -0300

    Adicionanando primeira atividade prática

---
+++ Int.Programação/Desenv.3/do-while.py
@@ -0,0 +1,9 @@
+i = 20
+while True:
+    if i < 0:
+        break
+    if i==13:
+        i = i - 1
+        continue
+    print (i)
+    i = i - 1
---
+++ Int.Programação/Desenv.3/for.py
@@ -0,0 +1,4 @@
+for i in range(20, 0, -1):
+    print(i)
+    if(i==13):
+        continue
