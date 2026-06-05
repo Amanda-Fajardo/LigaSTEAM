900008846efa56374dc7420dcf3f8aa5b998bb3c
Amanda-Fajardo <amandafajardo.dev@gmail.com>
2026-06-04 21:01:07 -0300

    Atividade da segunda calculadora

---
+++ Int.Programação/calculadora2.py
@@ -0,0 +1,40 @@
+def soma(a, b):
+    resultado = a + b
+    return resultado
+
+def sub(a, b):
+    resultado = a - b
+    return resultado
+
+def div(a, b):
+    resultado = a / b
+    return resultado
+
+def mult(a, b):
+    resultado = a * b
+    return resultado
+
+c = -1
+
+while (c != 0):
+    print("-------------------------------------------------")
+    print("Calculadora em linguagem Python")
+    print("-------------------------------------------------")
+
+    a = int(input("Digite o primeiro número da nossa operação: "))
+    b = int(input("Digite o segundo número da nossa operação: "))
+    c = int(input("Escolha a operação que deseja fazer: 1 - soma; 2 - subtração; 3 - divisão; 4 - multiplicação; 0 - Sair\n"))
+
+    if (c == 1):
+        print("Resultado: ",soma(a, b))
+    elif (c == 2):
+        print("Resultado: ",sub(a, b))
+    elif (c == 3):
+        print("Resultado: ",div(a, b))
+    elif (c == 4):
+        print("Resultado: ",mult(a, b))
+    elif (c == 0):
+        print("Encerrando calculadora...")
+        break
+
+    
